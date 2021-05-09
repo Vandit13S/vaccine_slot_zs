d01d6cbf87cd46562b09e8126f1f77f0ba0592d8
Vandit13S <32202175+Vandit13S@users.noreply.github.com>
2021-05-10 03:40:45 +0530

    Add files via upload

--- temp2.py
+++ temp2.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import  pandas as pd
 from bs4 import BeautifulSoup
 import time
 from webdriver_manager.chrome import ChromeDriverManager
